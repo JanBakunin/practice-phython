5b79b45c73586bb8d0124240a6212b58bae98b4b
Jan <flush625@gmail.com>
2017-08-10 12:25:37 +0200

    Rock Paper Scissors

--- Excercise8_rockPaperScissors/rockPaperScisssors.py
+++ Excercise8_rockPaperScissors/rockPaperScisssors.py
@@ -1,6 +1,30 @@
-player1 = input("Player 1 choose your play:")
-while player1 != "p" or player1 != "s" or player1 != "r":
-    player1 = input("Player 1 choose your play:")
-player2 = input("Player 2 choose your play:")
-while player2 != "p" or player2 != "s" or player2 != "r":
-    player2 = input("Player 1 choose your play:")
+import sys
+
+user1 = input("What's your name?")
+user2 = input("And your name?")
+user1_answer = input("%s, do yo want to choose rock, paper or scissors?" % user1)
+user2_answer = input("%s, do you want to choose rock, paper or scissors?" % user2)
+
+def compare(u1, u2):
+    if u1 == u2:
+        return("It's a tie!")
+    elif u1 == 'rock':
+        if u2 == 'scissors':
+            return("Rock wins!")
+        else:
+            return("Paper wins!")
+    elif u1 == 'scissors':
+        if u2 == 'paper':
+            return("Scissors win!")
+        else:
+            return("Rock wins!")
+    elif u1 == 'paper':
+        if u2 == 'rock':
+            return("Paper wins!")
+        else:
+            return("Scissors win!")
+    else:
+        return("Invalid input! You have not entered rock, paper or scissors, try again.")
+        sys.exit()
+
+print(compare(user1_answer, user2_answer))
